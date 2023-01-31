cc2fe64181f64fa14a3457c71c367366a911a89b
Nikhil4474 <ninuganti15@gmail.com>
2023-01-31 10:46:53 -0600

    trying to run swtich pipline via y

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -4,19 +4,42 @@
 
 package frc.robot.subsystems;
 
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.PS4Controller.Button;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 
-public class Limelight extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
-  public Limelight() {}
+// Test code to change between pipelines on limelight
+public class Limelight extends InstantCommand {
+  private NetworkTable limelight;
+  private int pipelineIndex = 0;
+  private Joystick joystick;
+  private JoystickButton ybutton;
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+  public Limelight(){
+    limelight = NetworkTableInstance.getDefault().getTable("limelight");
+    joystick = new Joystick(0);
+    ybutton = new JoystickButton(joystick, 4);
+    // ybutton.onTrue(switchPipeline());
+    // ybutton.whenPressed(new InstantCommand(switchPipeline()))
+    // CommandXboxController controllercommand = new CommandXboxController(1);
+    // Trigger xButton = controllercommand.x();
+
   }
 
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
+  public void switchPipeline(){
+    pipelineIndex = (pipelineIndex + 1) % 2;
+    limelight.getEntry("pipeline").setNumber(pipelineIndex);
+  }
+
+  public int getPipeLineIndex(){
+    return pipelineIndex;
   }
 }
