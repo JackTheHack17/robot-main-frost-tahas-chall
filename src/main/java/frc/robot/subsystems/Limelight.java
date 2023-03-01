d0d5c466eae6d4ea928111b0a34e88becdc9b303
RDc12 <52055654+RDc12Programming@users.noreply.github.com>
2023-02-28 21:51:19 -0600

    Add Limelight config TODO

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -46,6 +46,7 @@ public class Limelight extends SubsystemBase {
   }
 
   public Pose2d getPose() {
+    // TODO setup limelight botpose
     posevalues = limelight.getEntry("botpose").getDoubleArray(new double[6]);
     Translation2d translate = new Translation2d(posevalues[0], posevalues[1]);
     Rotation2d rotation = new Rotation2d(Math.toRadians(posevalues[3]));
