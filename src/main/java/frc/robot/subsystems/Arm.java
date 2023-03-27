bc797959f1c27967574783bcd2dbfb5599ff0d2b
AnshulBompada <115746973+AnshulBompada@users.noreply.github.com>
2023-03-26 23:20:19 -0500

    Fixed missing import caused by github

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -46,6 +46,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RepeatCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.ArmPosition;
